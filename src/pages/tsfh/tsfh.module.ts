43c4c0c7c733faed72715fedaa26ccf984b8f32e
SofiaVA43 <sofia-vargas2001@hotmail.com>
2018-10-23 09:05:21 -0500

    Se anadio contenido de pagina de imagen de album

---
+++ src/pages/tsfh/tsfh.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TsfhPage } from './tsfh';
+
+@NgModule({
+  declarations: [
+    TsfhPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TsfhPage),
+  ],
+})
+export class TsfhPageModule {}
